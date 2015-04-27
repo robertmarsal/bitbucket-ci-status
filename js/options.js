5ab581c0bfd03e7b84a6147189726d53e834d27d
Robert Boloc <robertboloc@gmail.com>
2015-04-27 21:08:38 +0100

    Functional options saving and retrieving

--- js/options.js
+++ js/options.js
@@ -41,11 +41,32 @@ function deleteOptions() {
  * Stores the new options.
  */
 function saveOptions() {
-    var shippableProjectId = document.getElementById('shippableProjectId').value;
 
-    // Store the option
+    var options = {};
+
+    var optionsRows = document.getElementsByClassName('option');
+
+    var provider, repo, extra;
+
+    for (var i = 0; i < optionsRows.length; i++) {
+
+        provider = optionsRows[i].children[0].textContent;
+        repo     = optionsRows[i].children[1].textContent;
+        extra    = optionsRows[i].children[2].textContent;
+
+        if (!options[repo]) {
+            options[repo] = [];
+        }
+
+        options[repo].push({
+            provider: provider,
+            extra: extra
+        })
+    }
+
+    // Store the options
     chrome.storage.sync.set({
-        shippableProjectId: shippableProjectId
+        options: options
     }, function() {
         // Display feedback
         var status = document.getElementById('save');
@@ -60,16 +81,33 @@ function saveOptions() {
  * Restores the already saved options.
  */
 function restoreOptions() {
-    chrome.storage.sync.get({
-        shippableProjectId: 'Project ID'
-    }, function(items) {
-        document.getElementById('shippableProjectId').value = items.shippableProjectId;
+    chrome.storage.sync.get("options", function(items) {
+
+        var tableBody = document.getElementById('options-tbody');
+
+        for (var repo in items.options) {
+            if (items.options.hasOwnProperty(repo)) {
+                for (provider in items.options[repo]) {
+
+                    var tr = document.createElement('tr');
+                    tr.className = 'option';
+                    tr.innerHTML = '' +
+                    '<td>' + String(items.options[repo][provider].provider) + '</td>' +
+                    '<td>' + String(repo) + '</td>' +
+                    '<td>' + String(items.options[repo][provider].extra) + '</td>' +
+                    '<td><button class="delete">Delete</button></td>';
+
+                    tableBody.insertBefore(tr, tableBody.children[tableBody.children.length - 1]);
+                }
+            }
+        }
+
+        attachDeleteListeners(); // Re-attach the delete event listeners
     });
 }
-/*
+
 // Attach the document ready listener
 document.addEventListener('DOMContentLoaded', restoreOptions);
-*/
 
 function attachDeleteListeners() {
     var deleteButtons = document.getElementsByClassName('delete');
